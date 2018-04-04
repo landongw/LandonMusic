131762403f8aa63703566a344a8c85f6c2333ca7
landongw <landon@extantpublishing.com>
2018-04-04 16:50:37 -0500

    Add new onCreate method to service

--- app/src/main/java/wiedenman/com/landonmusic/DownloadService.java
+++ app/src/main/java/wiedenman/com/landonmusic/DownloadService.java
@@ -3,6 +3,7 @@ package wiedenman.com.landonmusic;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
@@ -10,23 +11,26 @@ public class DownloadService extends Service {
 
     private static final String TAG = DownloadService.class.getSimpleName();
 
+    private DownloadHandler mHandler;
+
+    @Override
+    public void onCreate() {
+        DownloadThread thread = new DownloadThread();
+        thread.setName("Download Thread");
+        thread.start();
+        while (thread.mHandler == null) {
+        }
+        mHandler = thread.mHandler;
+
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         String song = intent.getStringExtra(MainActivity.KEY_SONG);
-        downloadSong(song);
+        Message message = Message.obtain();
+        message.obj = song;
+        mHandler.sendMessage(message);
         return Service.START_REDELIVER_INTENT;
-    }
-
-    private void downloadSong(String song) {
-        long endTime = System.currentTimeMillis() + 10*1000;  // simulates a 10 second download
-        while (System.currentTimeMillis() < endTime) {
-            try {
-                Thread.sleep(1000);// wait one second before looping
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        Log.d(TAG, song + " Song downloaded!");
     }
 
     @Nullable
